6038ccdf0f765a92fd49910d375124d2065a12d7
git <489548322@qq.com>
2019-01-14 16:46:15 +0800

    20190114

---
+++ roles/jdk/files/jdk1.8.0/java.sh
@@ -0,0 +1,6 @@
+export JAVA_HOME=/usr/local/java
+export JRE_HOME=/usr/local/java/jre
+export JAVA_BIN=$JAVA_HOME/bin
+export CLASSPATH=.:$JAVA_HOME/lib/dt.jar:$JAVA_HOME/lib/tools.jar
+export PATH=$JAVA_HOME/bin:$JRE_HOME/bin:$PATH
+
